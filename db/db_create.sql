4d4fe29e81165452f972c28e23b9b459323394f3
Bernd Fix <brf@hoi-polloi.org>
2021-03-15 21:03:33 +0100

    Reworked table and view layout.

--- db/db_create.sql
+++ db/db_create.sql
@@ -1,6 +1,6 @@
 -- ---------------------------------------------------------------------
 -- This file is part of 'bitbank-relay'.
--- Copyright (C) 2021 Bernd Fix >Y<
+-- Copyright (C) 2021 Bernd Fix   >Y<
 --
 -- 'bitbank-relay' is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as published
@@ -25,7 +25,7 @@ use BB_Relay;
 
 -- create user
 drop user if exists 'bb_relay';
-create user 'bb_relay'@'%' identified by 'bb_relay';
+create user 'bb_relay' @'%' identified by 'bb_relay';
 grant all on BB_Relay.* to bb_relay;
 flush privileges;
 
@@ -33,53 +33,125 @@ flush privileges;
 -- create tables
 -- ---------------------------------------------------------------------
 
+-- coin describes a cryptocurrency accepted by the relay
 create table coin (
-    id      integer auto_increment primary key, -- database record id
-    ref     integer not null,                   -- coin identifier
-    symbol  varchar(7) not null,                -- coin symbol (lowercase)
-    descr   varchar(63) default null,           -- coin name
-    logo    text default null,                  -- coin logo (base64-encoded SVG)
-    active  boolean default 0                   -- coin accepted?
+    -- database record id
+    id integer auto_increment primary key,
+
+    -- coin symbol (lowercase short name)
+    symbol varchar(7) not null,
+
+    -- coin long name / description
+    label varchar(63) default null,
+
+    -- coin logo (base64-encoded SVG)
+    logo text default null
 );
 
+-- account is a receiver for cryptocoins
 create table account (
-    id      integer auto_increment primary key, -- database record id
-    ref     varchar(7) not null,                -- account short reference
-    label   varchar(127) not null,              -- account label
-    active  boolean default 0                   -- account accepted?
+    -- database record id
+    id integer auto_increment primary key,
+
+    -- account label
+    label varchar(31) not null
 );
 
+-- accept list all account/coin pairs that can be processed
 create table accept (
     accnt integer references account(id) on delete cascade,
-    coin  integer references coin(id) on delete cascade
+    coin integer references coin(id) on delete cascade
 );
 
+-- id-less view on account/coin pairs that are accepted
 create view coins4account as select
-    c.symbol as symb,
-    c.descr as label,
+    c.symbol as coin,
+    c.label as label,
     c.logo as logo,
-    a.ref as ref,
     a.label as account
 from
-    coin c, account a, accept x 
+    coin c, account a, accept x
 where
     x.accnt = a.id and x.coin = c.id;
 
+-- addr is a cryptocurrency address that can receive coins
 create table addr (
-    id          integer auto_increment primary key,
-    coin        integer references coin(id) on delete cascade,
-    idx         integer not null,
-    val         varchar(127) not null,
-    stat        integer default 0,
-    firstSeen   timestamp default current_timestamp,
-    lastSeen    timestamp null default null
+    -- database record id
+    id integer auto_increment primary key,
+
+    -- associated coin
+    coin integer references coin(id) on delete cascade,
+
+    -- BIP32/39/44 address index
+    idx integer not null,
+
+    -- address as string
+    val varchar(127) not null,
+
+    -- status:
+    --  0 = unused
+    --  1 = pending (used in a pending transaction)
+    --  2 = closed (address was used; don't use again)
+    stat integer default 0,
+
+    -- reference to account (if stat != 0)
+    accnt integer references account(id) on delete cascade,
+
+    -- reference count in pending transactions:
+    refCnt integer default 0,
+
+    -- address life-span
+    validFrom timestamp default current_timestamp,
+    validTo timestamp null default null
 );
 
+-- id-less view on address records
+create view v_addr as select
+    c.label as coin,
+    a.val as val,
+    a.stat as stat,
+    b.label as account,
+    a.refCnt as cnt,
+    a.validFrom as validFrom,
+    a.validTo as validTo
+from
+    addr a, account b, coin c
+where
+    a.coin = c.id and a.accnt = b.id;
+
+
+-- transaction
 create table tx (
-    id          integer auto_increment primary key,
-    txid        varchar(64),
-    addr        integer references addr(id) on delete cascade,
-    accnt       integer references account(id) on delete cascade,
-    validFrom   integer not null,
-    validTo     integer not null
+    -- database record id
+    id integer auto_increment primary key,
+
+    -- 256-bit transaction identifier
+    txid varchar(64),
+
+    -- reference to address used in transaction
+    addr integer references addr(id) on delete cascade,
+
+    -- status:
+    --  0 = pending
+    --  1 = expired
+    --  2 = finalized
+    stat integer default 0,
+
+    -- transaction life-span
+    validFrom integer not null,
+    validTo integer not null
 );
+
+-- id-less view on a transaction
+create view v_tx as select
+    t.txid as txid,
+    a.val as addr,
+    c.symbol as coin,
+    b.label as account,
+    t.stat as stat,
+    t.validFrom as validFrom,
+    t.validTo as validTo
+from
+    tx t, addr a, account b, coin c
+where
+    t.addr = a.id and a.accnt = b.id and a.coin = c.id;
