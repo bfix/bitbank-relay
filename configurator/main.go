ffbcbe24f9bd1cd2ed42f9a1535969f7c9f80e08
Bernd Fix <brf@hoi-polloi.org>
2022-01-28 17:58:22 +0100

    Use Trezpr device for auto-configuration.

--- configurator/main.go
+++ configurator/main.go
@@ -31,6 +31,7 @@ import (
 	"os"
 	"relay/lib"
 
+	trezor "github.com/bfix/bitbank-trezor"
 	"github.com/bfix/gospel/bitcoin/wallet"
 	"github.com/bfix/gospel/logger"
 )
@@ -42,10 +43,10 @@ var fsys embed.FS
 var Version string = "v0.0.0"
 
 func main() {
-	fmt.Println("=================================")
+	fmt.Println("======================================")
 	fmt.Println("bitbank-relay-configurator " + Version)
-	fmt.Println("(c) 2021, Bernd Fix           >Y<")
-	fmt.Println("=================================")
+	fmt.Println("(c) 2021-2022, Bernd Fix           >Y<")
+	fmt.Println("======================================")
 
 	// parse and process command-line options
 	var (
@@ -53,11 +54,13 @@ func main() {
 		inConf  string
 		outConf string
 		export  bool
+		mode    string
 	)
 	flag.BoolVar(&export, "export", false, "Export embedded files")
 	flag.StringVar(&network, "n", "main", "Network [main|test|reg]")
 	flag.StringVar(&inConf, "i", "", "Configuration template file (default: embedded config)")
 	flag.StringVar(&outConf, "o", "config.json", "Configuration output file (default: config.json)")
+	flag.StringVar(&mode, "m", "trezor", "Configuration mode (trezor, seed)")
 	flag.Parse()
 
 	// special function "export embedded files"
@@ -86,47 +89,12 @@ func main() {
 		}
 		return
 	}
-
-	// Ask for passphrase
-	// N.B.: This is not a BIP39 password added to the list of seed words,
-	// but a passphrase used to generate the seed words for a BIP39 wallet.
-	fmt.Printf(">>> Passphrase: ")
-	rdr := bufio.NewReader(os.Stdin)
-	in, _, err := rdr.ReadLine()
-	if err != nil {
-		fmt.Println("<<< ERROR: " + err.Error())
-		return
-	}
-	// compute entropy, seed words and seed value
-	ent := sha256.Sum256(in)
-	words, err := wallet.EntropyToWords(ent[:])
-	if err != nil {
-		fmt.Println("<<< ERROR: " + err.Error())
-		return
-	}
-	seed, _ := wallet.WordsToSeed(words, "")
-
-	// output computed information
-	fmt.Printf("<<<    Entropy: %s\n", hex.EncodeToString(ent[:]))
-	fmt.Printf("<<<       Seed: %s\n", hex.EncodeToString(seed))
-	fmt.Println("<<<------------------------------------------------")
-	fmt.Println("<<< Seed words:")
-	n := len(words) / 2
-	for i := 0; i < n; i++ {
-		fmt.Printf("<<<    %2d: %-20s %2d: %-20s\n", i+1, words[i], i+n+1, words[i+n])
-	}
-	fmt.Println("<<<------------------------------------------------")
-
-	// create a HD wallet for the given seed
-	hd := wallet.NewHD(seed)
-	pk := hd.MasterPublic()
-	fmt.Printf("<<< Master Pub: %s\n", pk)
-	sk := hd.MasterPrivate()
-	fmt.Printf("<<< Master Prv: %s\n", sk)
-
 	// load config template
 	fmt.Println("<<< Generate configuration file...")
-	var cfg *lib.Config
+	var (
+		cfg *lib.Config
+		err error
+	)
 	if len(inConf) > 0 {
 		cfg, err = lib.ReadConfigFile(inConf)
 	} else {
@@ -139,38 +107,116 @@ func main() {
 		fmt.Println("<<< ERROR: " + err.Error())
 		return
 	}
-	// process all entries
-	netw := lib.GetNetwork(network)
-	for _, coin := range cfg.Coins {
-		fmt.Printf("<<<    Processing '%s'...\n", coin.Symb)
 
-		// get base extended public key for given account
-		bpk, err := hd.Public(coin.Path)
+	// generate data based on configuration mode
+	if mode == "seed" {
+		// Seed-based configuration
+		// ========================
+		// Ask for passphrase
+		// N.B.: This is not a BIP39 password added to the list of seed words,
+		// but a passphrase used to generate the seed words for a BIP39 wallet.
+		fmt.Printf(">>> Passphrase: ")
+		rdr := bufio.NewReader(os.Stdin)
+		in, _, err := rdr.ReadLine()
 		if err != nil {
 			fmt.Println("<<< ERROR: " + err.Error())
-			continue
+			return
 		}
-		bpk.Data.Version = coin.GetXDVersion()
-		coin.Pk = bpk.String()
-
-		// get coin handler
-		hdlr, err := lib.NewHandler(coin, netw)
+		// compute entropy, seed words and seed value
+		ent := sha256.Sum256(in)
+		words, err := wallet.EntropyToWords(ent[:])
 		if err != nil {
 			fmt.Println("<<< ERROR: " + err.Error())
-			continue
+			return
 		}
+		seed, _ := wallet.WordsToSeed(words, "")
 
-		// compute addresses; save first for check
-		for idx := 0; idx < 10; idx++ {
-			addr, err := hdlr.GetAddress(idx)
+		// output computed information
+		fmt.Printf("<<<    Entropy: %s\n", hex.EncodeToString(ent[:]))
+		fmt.Printf("<<<       Seed: %s\n", hex.EncodeToString(seed))
+		fmt.Println("<<<------------------------------------------------")
+		fmt.Println("<<< Seed words:")
+		n := len(words) / 2
+		for i := 0; i < n; i++ {
+			fmt.Printf("<<<    %2d: %-20s %2d: %-20s\n", i+1, words[i], i+n+1, words[i+n])
+		}
+		fmt.Println("<<<------------------------------------------------")
+
+		// create a HD wallet for the given seed
+		hd := wallet.NewHD(seed)
+		pk := hd.MasterPublic()
+		fmt.Printf("<<< Master Pub: %s\n", pk)
+		sk := hd.MasterPrivate()
+		fmt.Printf("<<< Master Prv: %s\n", sk)
+
+		// process all entries
+		netw := lib.GetNetwork(network)
+		for _, coin := range cfg.Coins {
+			fmt.Printf("<<<    Processing '%s'...\n", coin.Symb)
+
+			// get base extended public key for given account
+			bpk, err := hd.Public(coin.Path)
 			if err != nil {
 				fmt.Println("<<< ERROR: " + err.Error())
 				continue
 			}
-			if idx == 0 {
-				coin.Addr = addr
+			bpk.Data.Version = coin.GetXDVersion()
+			coin.Pk = bpk.String()
+
+			// get coin handler
+			hdlr, err := lib.NewHandler(coin, netw)
+			if err != nil {
+				fmt.Println("<<< ERROR: " + err.Error())
+				continue
 			}
-			fmt.Printf("<<<    %2d: %s\n", idx, addr)
+
+			// compute addresses; save first for check
+			for idx := 0; idx < 10; idx++ {
+				addr, err := hdlr.GetAddress(idx)
+				if err != nil {
+					fmt.Println("<<< ERROR: " + err.Error())
+					continue
+				}
+				if idx == 0 {
+					coin.Addr = addr
+				}
+				fmt.Printf("<<<    %2d: %s\n", idx, addr)
+			}
+		}
+	} else if mode == "trezor" {
+		// Trezor-based configuration
+		// ==========================
+		ce := new(trezor.ConsoleEntry)
+		trezor, err := trezor.OpenTrezor(ce)
+		if err != nil {
+			fmt.Println("<<< ERROR: " + err.Error())
+			return
+		}
+		if trezor == nil {
+			fmt.Println("<<< ERROR: no Trezor device found!")
+			return
+		}
+		defer trezor.Close()
+
+		// give info on Trezor device
+		fmt.Println("<<< Trezor connected:")
+		fw := trezor.Firmware()
+		fmt.Printf("<<<     Firmare: %d.%d.%d\n", fw[0], fw[1], fw[2])
+		fmt.Printf("<<<       Label: '%s'\n", trezor.Label())
+
+		for _, coin := range cfg.Coins {
+			fmt.Printf("<<<    Processing '%s'...\n", coin.Symb)
+
+			// get public master
+			if coin.Pk, err = trezor.GetXpub(coin.Path, coin.Symb, coin.Mode); err != nil {
+				fmt.Println("<<< ERROR: " + err.Error())
+				continue
+			}
+			// get first address
+			if coin.Addr, err = trezor.GetAddress(coin.Path, coin.Symb, coin.Mode); err != nil {
+				fmt.Println("<<< ERROR: " + err.Error())
+				continue
+			}
 		}
 	}
 	// save to configuration file
